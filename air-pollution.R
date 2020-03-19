49873b84d829e55b15a7bdab225a628b691769ed
babuaravind <itsbabuaravind@gmail.com>
2020-03-19 22:10:29 +0530

    Added Complete Observable and Correlation function.

--- air-pollution.R
+++ air-pollution.R
@@ -1,3 +1,7 @@
+## -------------------------------------------------
+## Returns the specified pollutant's mean value.
+## -------------------------------------------------
+
 pollutantmean <- function(directory, pollutant, id=1:332) {
   # Free variables
   dataframe <- 0
@@ -6,7 +10,7 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
     
     index <- 0
     
-    # repending '00' or '0' to files.
+    # Prepending '00' or '0' to files.
     if(i <10 ) {
       index = (paste("00", as.character(i), sep=""))
     } else if(i < 100) {
@@ -30,11 +34,52 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   return(result)
 }
 
+# Function call
 pollutantmean('specdata', 'nitrate', 70:72)
 
-############################
 
-complete <- function(directory, pollutant, id=1:332) {
+## -------------------------------------------------
+## Returns the count of completely observed cases in specified data file(s).
+## -------------------------------------------------
+
+complete <- function(directory, id=1:332) {
+  # Free variables
+  df <- data.frame(id=numeric(), nobs=numeric())
+  names(df)<-c("id","nobs")
+  
+  for(i in id) {
+    
+    index <- 0
+    
+    # Prepending '00' or '0' to files.
+    if(i <10 ) {
+      
+      index = (paste("00", as.character(i), sep=""))
+    } else if(i < 100) {
+      index = (paste("0", as.character(i), sep=""))
+    } else {
+      index = (paste("", as.character(i), sep=""))
+    }
+    
+    dir <- paste(directory, "/", index, ".csv", sep="")
+    data <- read.csv(dir, header=T)
+    
+    x <- data[complete.cases(data), ]
+    nobs <- nrow(x)
+    de <- data.frame(i, nobs)
+    df <- rbind(df,de)
+  }
+  return(df)
+}
+# Function call
+complete('specdata', c(2,4,8,10,12))
+
+
+## -------------------------------------------------
+## Corelation between two vectors
+## -------------------------------------------------
+
+corr <- function(directory, pollutant, id=1:332) {
   # Free variables
   dataframe <- 0
   
@@ -42,7 +87,7 @@ complete <- function(directory, pollutant, id=1:332) {
     
     index <- 0
     
-    # repending '00' or '0' to files.
+    # Prepending '00' or '0' to files.
     if(i <10 ) {
       index = (paste("00", as.character(i), sep=""))
     } else if(i < 100) {
@@ -51,7 +96,7 @@ complete <- function(directory, pollutant, id=1:332) {
       index = (paste("", as.character(i), sep=""))
     }
     
-    dir <- paste('specdata', "/", index, ".csv", sep="")
+    dir <- paste(directory, "/", index, ".csv", sep="")
     data <- read.csv(dir, header=T)
     
     # Merging dataframes rowwise
